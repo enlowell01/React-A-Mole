a0b7b94ab0a20ed9b454eb97290f02826746686f
Evan <evanlowell01@gmail.com>
2023-06-01 15:00:43 -0400

    Create initial functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import MoleContainer from './MoleContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    let [score, setScore] = useState(0)
+
+    const createMoleHill = () => {
+        let hills = []
+        for (let i = 0; i < 9; i++) {
+            hills.push(
+                <MoleContaine key={i} setScore={setScore} score={score} />)
+        }
+    
+        return (
+            <div>
+                {hills}
+            </div>
+        )
+    }
+
+    return (
+        <div className="App">
+            <h1>React-a-Mole!</h1>
+            {score}
+            {createMoleHill()}
+        </div>
+    )
 }
 
-export default App;
+export default App
